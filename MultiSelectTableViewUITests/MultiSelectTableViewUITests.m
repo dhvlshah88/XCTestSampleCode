6ea3688602a4b5cae772953ded36494575d918ef
Dhaval Shah <dhvlshah88@gmail.com>
2016-09-14 14:06:40 -0700

    Added two test cases
    Test Case 1 - Delete all the default 12 items:
    1. Launch the app
    2. Click ‘Edit’ button
    3. Select ‘Delete All’
    4. Click ‘OK’ in the popup to confirm remove all items
    5. Verify there is no items listed, and ‘Edit’ button is grayed out
    
    Test Case 2 - Add 3 New Items and delete 2 of them
    1. Click the plus button 3 times to add 3 New Item
    2. Click ‘Edit’ button
    3. Select 2 of the newly added item
    4. Verify it shows ‘Delete (2)’ in the navigation bar
    5. Click on ‘Delete (2)’
    6. Click ‘OK’ in the popup to confirm
    7. Verify the selected two items are removed

--- MultiSelectTableViewUITests/MultiSelectTableViewUITests.m
+++ MultiSelectTableViewUITests/MultiSelectTableViewUITests.m
@@ -32,9 +32,83 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testDeleteAll {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *masterNavigationBar = app.navigationBars[@"Master"];
+    [masterNavigationBar.buttons[@"Edit"] tap];
+    [masterNavigationBar.buttons[@"Delete All"] tap];
+    [app.sheets[@"Are you sure you want to remove these items?"].buttons[@"OK"] tap];
+    
+    // an assert to check if the final count of items in table after Delete All is 0
+    XCTAssertEqual(app.tables.cells.count, 0, @"Expected Result: 0");
+    
+    //a method call to testDisabledElement to check is item is disabled
+    [self testDisabledElement:masterNavigationBar.buttons[@"Edit"]];
+}
+
+- (void)testDisabledElement:(XCUIElement *)element {
+    // an assert to check if an element is disabled
+    XCTAssertEqual(element.isEnabled, false, @"Expected Result: False \n Element should be disabled");
+}
+
+- (void)testAddAndRemove {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *masterNavigationBar = app.navigationBars[@"Master"];
+    XCUIElement *editButton = masterNavigationBar.buttons[@"Edit"];
+    XCUIElement *addButton = masterNavigationBar.buttons[@"Add"];
+    XCUIElement *okButton = app.sheets[@"Are you sure you want to remove these items?"].buttons[@"OK"];
+    
+    // get total number of items in table before adding 3 new items
+    NSUInteger orginalTotalRecords = app.tables.cells.count;
+    
+    //add three items to table.
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    
+    // get total number of items in table after adding 3 new items
+    NSUInteger currentTotalRecords = app.tables.cells.count;
+    
+    XCTAssertEqual(currentTotalRecords - orginalTotalRecords, 3, @"Expected Result: 3");
+    
+    [editButton tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:5] swipeUp];
+    
+    //check if the cell exists before selecting
+    XCTAssert([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:12].exists, @"Expected Result: True");
+    XCTAssert([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:13].exists, @"Expected Result: True");
+    
+    [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:13].staticTexts[@"New Item"] tap];
+    
+    //check if the cell is actually selected
+    XCTAssertTrue([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:13].isSelected, @"Expected Result: True");
+    
+    //check if the cell exists before selecting
+    XCTAssert([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:14].exists, @"Expected Result: True");
+    
+    [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:14].staticTexts[@"New Item"] tap];
+    
+    //check if the cell is actually selected
+    XCTAssertTrue([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:14].isSelected, @"Expected Result: True");
+    
+    //check if the cell is actually selected
+    XCTAssertEqual([[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:12].isSelected, false, @"Expected Result: False");
+    
+    //check if two items were selected
+    XCTAssert(masterNavigationBar.buttons[@"Delete (2)"].exists, @"Expected Result: True");
+    
+    XCUIElement *deleteButton = masterNavigationBar.buttons[@"Delete (2)"];
+    
+    NSString *expectedText = @"Delete (2)";
+    XCTAssertTrue([deleteButton.label isEqualToString:expectedText], @"Expected Result: button text %@ should be same as %@", deleteButton.label, expectedText);
+    [deleteButton tap];
+    [okButton tap];
+    
+    // get total number of items in table after removing 2 items
+    XCTAssertEqual(currentTotalRecords - app.tables.cells.count, 2, @"Expected Result: 2");
 }
 
 @end
